f9fbc052b4043d8efa59e2faf3c7abb50f95c2ef
Ricardo Freitas <derfreitas@gmail.com>
2016-07-26 11:31:39 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import Onfido from 'onfido-sdk-ui'
+
+
+window.onload = function() {
+  var url = "https://gentle-gorge-17630.herokuapp.com/api"
+  var request = new XMLHttpRequest()
+  request.open('GET', url, true)
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      var data = JSON.parse(request.responseText)
+
+      Onfido.init({
+        useModal: false,
+        token: data.message,
+        onReady: function() {
+          // callback that fires when successfully authorised
+        },
+        onDocumentCapture: function(event) {
+          // callback for when the document has captured successfully
+        },
+        onFaceCapture: function(event) {
+          // callback for when the face capture was successful
+        },
+        onComplete: function(event) {
+          // callback for when everything is complete
+        },
+        steps: [
+          {
+            type:'welcome',
+            options:{
+              title:'Open your new bank account'
+            }
+          },
+          'document',
+          'face',
+          'complete'
+        ]
+      });
+    }
+  }
+  request.send()
+};
