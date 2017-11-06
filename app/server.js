6ae9be8c6252c909673b9d47454931227336eced
Ricardo Freitas <derfreitas@gmail.com>
2017-11-06 13:25:19 +0000

    server clean up: removed webpack dev server

--- app/server.js
+++ app/server.js
@@ -6,32 +6,31 @@ import { spawn } from 'child_process';
 import url from 'url';
 import secrets from '../secrets.json';
 import unirest from 'unirest';
-const USE_WEBPACK_DEVSERVER = false;//helpful for live reloading
 
-var apiToken = secrets.apiToken;
-var PORT = 8090;
+const apiToken = secrets.apiToken;
+const PORT = 8090;
 
-var onfidoRequest = (method, path, parameters, callback) => {
+const onfidoRequest = (method, path, parameters, callback) => {
   unirest[method]('https://api.onfido.com/v2/'+path)
     .headers({'Authorization': 'Token token='+apiToken})
     .send(parameters)
     .end(callback)
 };
 
-var getToken = function(token, originalRes){
-  createApplicant( ({body:applicant}) => {
-    onfidoRequest('post','sdk_token',
-      {referrer:"*://*/*", applicant_id:applicant.id},
-      ({body})=> originalRes.send(body)
-    )
-  })
-};
-
-var createApplicant = function(callback){
+const createApplicant = function(callback){
   onfidoRequest('post', 'applicants', {first_name:"John",last_name:"Doe"}, callback)
 };
 
-var isOriginHostnameSameAsServer = function(req){
+const createJWT = (callback) =>{
+  createApplicant( ({body:applicant}) => {
+    onfidoRequest('post','sdk_token',
+      {referrer:"*://*/*", applicant_id:applicant.id},
+      callback
+    )
+  })
+}
+
+const isOriginHostnameSameAsServer = function(req){
   var origin = req.get('origin');
   if (!origin) return;
 
@@ -42,15 +41,15 @@ var isOriginHostnameSameAsServer = function(req){
 const certPath = 'cert.pem';
 const keyPath = 'key.pem';
 
-var startServer = function(key,cert){
+const startServer = function(key,cert){
   https.createServer({key: key, cert: cert}, app).listen(PORT);
   console.log("listening on "+PORT);
 };
 
-var getKeys = function(callback){
+const getKeys = function(callback){
   try {
-    var cert = fs.readFileSync(certPath, 'utf8');
-    var key = fs.readFileSync(keyPath, 'utf8');
+    const cert = fs.readFileSync(certPath, 'utf8');
+    const key = fs.readFileSync(keyPath, 'utf8');
     callback(key, cert);
   } catch (e) {
     console.log("generating keys");
@@ -62,30 +61,16 @@ var getKeys = function(callback){
   }
 };
 
-var startServerSigned = function(){
+const startServerSigned = function(){
   getKeys(startServer);
 };
 
-var app = express();
+const app = express();
 
 app.get('/jwt', function (req, res) {
-  var origin = req.get('origin');
-
-  //allow Access-Control-Allow-Origin if the domain is the same but port might be different
-  if (isOriginHostnameSameAsServer(req)){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  }
-
-  getToken(apiToken, res);
+  createJWT(({body})=> res.send(body))
 });
 
 app.use(express.static('dist'));
 
 startServerSigned();
-
-if (USE_WEBPACK_DEVSERVER){
-  var child = spawn('npm',['run','dev:webpack']);
-  child.stdout.pipe(process.stdout);
-  child.stderr.pipe(process.stderr);
-}
